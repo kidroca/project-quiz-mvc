202d74c4d9c29af45f6eee4a8ad942e89a15cea0
Peter Velkov <kidroca@gmail.com>
2016-02-16 14:52:58 +0200

    Added methods for saving and evaluating a quiz in the quiz service interface and implementation

--- Services/QuizProjectMvc.Services.Data/IQuizesService.cs
+++ Services/QuizProjectMvc.Services.Data/IQuizesService.cs
@@ -3,10 +3,15 @@
     using System.Linq;
 
     using QuizProjectMvc.Data.Models;
+    using QuizProjectMvc.Web.Infrastructure.SharedModels;
 
     public interface IQuizesService
     {
         IQueryable<Quiz> GetRandomQuizzes(int count);
+
+        QuizEvaluationResult EvaluateSolution(QuizSolution quizSolution);
+
+        QuizSolution SaveSolution(SolutionForEvaluationModel quizSolution, Quiz quiz, string userId);
 
         Quiz GetById(string id);
     }
--- Services/QuizProjectMvc.Services.Data/QuizesService.cs
+++ Services/QuizProjectMvc.Services.Data/QuizesService.cs
@@ -1,10 +1,12 @@
 ﻿namespace QuizProjectMvc.Services.Data
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     using QuizProjectMvc.Data.Common;
     using QuizProjectMvc.Data.Models;
+    using QuizProjectMvc.Web.Infrastructure.SharedModels;
     using Web;
 
     public class QuizesService : IQuizesService
@@ -16,6 +18,65 @@
         {
             this.quizzes = quizzes;
             this.identifierProvider = identifierProvider;
+        }
+
+        public QuizEvaluationResult EvaluateSolution(QuizSolution quizSolution)
+        {
+            var result = new QuizEvaluationResult
+            {
+                ForQuizId = quizSolution.ForQuizId,
+                Title = quizSolution.ForQuiz.Title,
+                CorrectlyAnswered = new List<QuestionResultModel>(),
+                IncorrectlyAnswered = new List<QuestionResultModel>()
+            };
+
+            foreach (Answer answer in quizSolution.SelectedAnswers)
+            {
+                var questionResult = new QuestionResultModel
+                {
+                    Question = answer.ForQuestion.Title,
+                    IsCorrect = answer.IsCorrect,
+                    ResultDescription = answer.ForQuestion.ResultDescription,
+                    SelectedAnswer = answer.Text,
+                    CorrectAnswer = answer.ForQuestion
+                            .Answers.First(a => a.IsCorrect).Text
+                };
+
+                if (answer.IsCorrect)
+                {
+                    result.CorrectlyAnswered.Add(questionResult);
+                }
+                else
+                {
+                    result.IncorrectlyAnswered.Add(questionResult);
+                }
+            }
+
+            return result;
+        }
+
+        public QuizSolution SaveSolution(SolutionForEvaluationModel quizSolution, Quiz quiz, string userId)
+        {
+            var selectedAnswers = new List<Answer>();
+            foreach (var answeredQuestion in quizSolution.Questions)
+            {
+                selectedAnswers.Add(new Answer
+                {
+                    Id = answeredQuestion.SelectedAnswerId
+                });
+            }
+
+            var newSolution = new QuizSolution
+            {
+                ByUserId = userId,
+                CreatedOn = DateTime.Now,
+                SelectedAnswers = selectedAnswers,
+            };
+
+            quiz.Solutions.Add(newSolution);
+            this.quizzes.Save();
+
+            return newSolution;
         }
 
         public Quiz GetById(string id)
