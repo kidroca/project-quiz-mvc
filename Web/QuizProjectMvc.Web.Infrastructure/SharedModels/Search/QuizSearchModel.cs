68dfce7feb7c64d30e480a4c82e892a337699f31
Peter Velkov <kidroca@gmail.com>
2016-02-16 14:58:30 +0200

    Added a quiz search model

---
+++ Web/QuizProjectMvc.Web.Infrastructure/SharedModels/Search/QuizSearchModel.cs
@@ -0,0 +1,28 @@
+﻿namespace QuizProjectMvc.Web.Infrastructure.SharedModels.Search
+{
+    using System;
+
+    // Todo: Add Constraints
+    public class QuizSearchModel
+    {
+        public string Category { get; set; }
+
+        public string KeyPhrase { get; set; }
+
+        public double? MinRating { get; set; }
+
+        public double? MaxRating { get; set; }
+
+        public int? MinQuestions { get; set; }
+
+        public int? MaxQuestions { get; set; }
+
+        public DateTime? FromDate { get; set; }
+
+        public DateTime? ToDate { get; set; }
+
+        public ResultOrder? OrderBy { get; set; }
+
+        public bool OrderDescending { get; set; }
+    }
+}
---
+++ Web/QuizProjectMvc.Web.Infrastructure/SharedModels/Search/ResultOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizProjectMvc.Web.Infrastructure.SharedModels.Search
+{
+    public enum ResultOrder
+    {
+        ByDate = 0,
+        ByRating = 1,
+        ByNumberOfQuestions = 2,
+        ByTimesTaken = 3
+    }
+}
