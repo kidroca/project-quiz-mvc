1581ea79e861fe0d6d8ee06f8db0a2ec96b91f0a
Peter Velkov <kidroca@gmail.com>
2016-02-16 15:00:28 +0200

    Added solve action to the quizzes controller (incomplete)

--- Web/QuizProjectMvc.Web/Controllers/QuizzesController.cs
+++ Web/QuizProjectMvc.Web/Controllers/QuizzesController.cs
@@ -1,25 +1,48 @@
 ﻿namespace QuizProjectMvc.Web.Controllers
 {
     using System.Web.Mvc;
-
+    using Data.Models;
+    using Infrastructure.SharedModels;
+    using Infrastructure.SharedModels.Evaluation;
     using QuizProjectMvc.Services.Data;
-    using QuizProjectMvc.Web.ViewModels.Home;
+    using ViewModels.Quiz;
 
     public class QuizzesController : BaseController
     {
-        private readonly IQuizesService quizes;
+        private readonly IQuizesService quizzes;
 
         public QuizzesController(
-            IQuizesService quizes)
+            IQuizesService quizzes)
         {
-            this.quizes = quizes;
+            this.quizzes = quizzes;
         }
 
         public ActionResult ById(string id)
         {
-            var quiz = this.quizes.GetById(id);
+            var quiz = this.quizzes.GetById(id);
             var viewModel = this.Mapper.Map<QuizBasicViewModel>(quiz);
             return this.View(viewModel);
+        }
+
+        // Todo: Solve Action HttpGet
+        [HttpPost]
+        public ActionResult Solve(SolutionForEvaluationModel quizSolution)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                // Todo Retry!
+            }
+
+            var quiz = this.quizzes.GetById(quizSolution.ForQuizId);
+            if (quizSolution.Questions.Count != quiz.Questions.Count)
+            {
+                // return this.BadRequest("Invalid Solution: Questions count mismatch");
+            }
+
+            QuizSolution solution = this.quizzes.SaveSolution(quizSolution, quiz, this.UserId);
+            QuizEvaluationResult result = this.quizzes.EvaluateSolution(solution);
+
+            // Todo: Redirect with result
         }
     }
 }
