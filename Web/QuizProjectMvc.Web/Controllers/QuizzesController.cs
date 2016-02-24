159fff926ad4cecfe1ba9106b6963b0a380b4565
Peter Velkov <kidroca@gmail.com>
2016-02-24 16:46:08 +0200

    Separated Solve Actions from the QuizzesController

--- Web/QuizProjectMvc.Web/Controllers/QuizzesController.cs
+++ Web/QuizProjectMvc.Web/Controllers/QuizzesController.cs
@@ -14,32 +14,11 @@
             this.quizzes = quizzes;
         }
 
-        public ActionResult ById(string id)
+        public ActionResult ById(int id)
         {
             var quiz = this.quizzes.GetById(id);
             var viewModel = this.Mapper.Map<QuizBasicViewModel>(quiz);
             return this.View(viewModel);
         }
-
-        // Todo: Solve Action HttpGet
-        //[HttpPost]
-        //public ActionResult Solve(SolutionForEvaluationModel quizSolution)
-        //{
-        //    if (!this.ModelState.IsValid)
-        //    {
-        //        // Todo Retry!
-        //    }
-
-        //    var quiz = this.quizzes.GetById(quizSolution.ForQuizId);
-        //    if (quizSolution.Questions.Count != quiz.Questions.Count)
-        //    {
-        //        // return this.BadRequest("Invalid Solution: Questions count mismatch");
-        //    }
-
-        //    QuizSolution solution = this.quizzes.SaveSolution(quizSolution, quiz, this.UserId);
-        //    QuizEvaluationResult result = this.quizzes.EvaluateSolution(solution);
-
-        //    // Todo: Redirect with result
-        //}
     }
 }
--- Web/QuizProjectMvc.Web/Controllers/SolveQuizController.cs
+++ Web/QuizProjectMvc.Web/Controllers/SolveQuizController.cs
@@ -15,18 +15,39 @@
         }
 
         [HttpGet]
-        public ActionResult Solve(int id, string title)
+        public ActionResult Solve(int id)
         {
             var model = this.Mapper.Map<QuizForSolvingModel>(this.quizzes.GetById(id));
 
-            return View(model);
+            return this.View(model);
         }
 
         [HttpPost]
-        //[ValidateAntiForgeryToken]
         public ActionResult Solve(SolutionForEvaluationModel solution)
         {
-            return View();
+            return this.View();
         }
+
+
+        // Todo: Solve Action HttpGet
+        //[HttpPost]
+        //public ActionResult Solve(SolutionForEvaluationModel quizSolution)
+        //{
+        //    if (!this.ModelState.IsValid)
+        //    {
+        //        // Todo Retry!
+        //    }
+
+        //    var quiz = this.quizzes.GetById(quizSolution.ForQuizId);
+        //    if (quizSolution.Questions.Count != quiz.Questions.Count)
+        //    {
+        //        // return this.BadRequest("Invalid Solution: Questions count mismatch");
+        //    }
+
+        //    QuizSolution solution = this.quizzes.SaveSolution(quizSolution, quiz, this.UserId);
+        //    QuizEvaluationResult result = this.quizzes.EvaluateSolution(solution);
+
+        //    // Todo: Redirect with result
+        //}
     }
 }
