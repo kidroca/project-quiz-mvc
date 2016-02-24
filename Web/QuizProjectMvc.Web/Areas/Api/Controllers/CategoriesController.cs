b8a26766e182e942cdbe813c0fd40a726128f788
Peter Velkov <kidroca@gmail.com>
2016-02-24 16:44:35 +0200

    Added Categories Controller to the WebApi for Ajax requests

---
+++ Web/QuizProjectMvc.Web/Areas/Api/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿namespace QuizProjectMvc.Web.Areas.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using Services.Data;
+
+    public class CategoriesController : BaseController
+    {
+        private readonly ICategoriesService categories;
+
+        public CategoriesController(ICategoriesService categories)
+        {
+            this.categories = categories;
+        }
+
+        [OutputCache(Duration = 10 * 60)]
+        public IHttpActionResult GetByPattern(string pattern, int take = 10)
+        {
+            var result = this.categories.FilterByPattern(pattern, take)
+                .Select(c => c.Name)
+                .ToList();
+
+            return this.Ok(result);
+        }
+    }
+}
