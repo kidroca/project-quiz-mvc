29cfc8a832be9fcb573b06c156ff602db816c2e6
Peter Velkov <kidroca@gmail.com>
2016-02-24 16:45:15 +0200

    Created AjaxOnlyAction Filter attribute

---
+++ Web/QuizProjectMvc.Web/Helpers/Filters/AjaxOnlyAction.cs
@@ -0,0 +1,22 @@
+﻿namespace QuizProjectMvc.Web.Helpers.Filters
+{
+    using System;
+    using System.Web.Mvc;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AjaxOnlyAction : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.HttpContext.Response.StatusCode = 404;
+                filterContext.Result = new HttpNotFoundResult();
+            }
+            else
+            {
+                base.OnActionExecuting(filterContext);
+            }
+        }
+    }
+}
