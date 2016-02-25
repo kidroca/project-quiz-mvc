5a0fc049cd56f4db86d7fb99c69b3557c981f228
Peter Velkov <kidroca@gmail.com>
2016-02-25 20:56:52 +0200

    Added custom exceptions to the app

---
+++ Services/QuizProjectMvc.Services.Data/Exceptions/QuizCreationException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizProjectMvc.Services.Data.Exceptions
+{
+    using System;
+    public class QuizCreationException : ApplicationException
+    {
+        public QuizCreationException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ Services/QuizProjectMvc.Services.Data/Exceptions/QuizEvaluationException.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizProjectMvc.Services.Data.Exceptions
+{
+    using System;
+
+    public class QuizEvaluationException : ApplicationException
+    {
+        public QuizEvaluationException(string message)
+            : base(message)
+        {
+        }
+
+        public QuizEvaluationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
