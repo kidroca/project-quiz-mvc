8d153f7e5734f2566d919b05e7694a7590fbfba4
Peter Velkov <kidroca@gmail.com>
2016-02-25 20:52:12 +0200

    Added Entity Validation Helper To Merge Validation error to the exception

--- Data/QuizProjectMvc.Data/ApplicationDbContext.cs
+++ Data/QuizProjectMvc.Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Data.Entity.Validation;
     using System.Linq;
 
     using Common.Models;
@@ -42,7 +43,11 @@
             this.ApplyAuditInfoRules();
             this.DeleteOrphanQuestions();
 
+#if DEBUG
+            return this.DecorateValidationExceptionIfAny();
+#else
             return base.SaveChanges();
+#endif
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -98,6 +103,32 @@
                 .Where(q => q.Quiz == null)
                 .ToList()
                 .ForEach(q => this.Questions.Remove(q));
+        }
+
+        private int DecorateValidationExceptionIfAny()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(
+                    ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(
+                    exceptionMessage, ex.EntityValidationErrors);
+            }
         }
     }
 }
