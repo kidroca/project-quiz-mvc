cf68f3b433a528ed44e383e77b88eeab9c11b59d
Peter Velkov <kidroca@gmail.com>
2016-02-26 22:21:08 +0200

    Added logic for "My" Action in the profile controller

--- Web/QuizProjectMvc.Web/Controllers/ProfileController.cs
+++ Web/QuizProjectMvc.Web/Controllers/ProfileController.cs
@@ -1,18 +1,36 @@
 ﻿namespace QuizProjectMvc.Web.Controllers
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Mvc;
+    using Services.Data.Protocols;
     using ViewModels.ProfileInformation;
 
-    public class ProfileController : Controller
+    public class ProfileController : BaseController
     {
+        private readonly IUsersService users;
+
+        public ProfileController(IUsersService users)
+        {
+            this.users = users;
+        }
+
+        [Authorize]
         public ActionResult My()
         {
-            // Todo Get public profile information, last 10 quizzes created, last 10 quizzes solution
-            // and add the to the page view model then display, confirm the 'edit profile' button is showing
+            var user = this.users.ById(this.UserId);
+            var profileInfo = this.Mapper.Map<PublicProfileInformation>(user);
+            var lastQuizzes = this.Mapper.Map<List<CreatedQuizInfo>>(user.QuizzesCreated.Take(10));
+            var lastSolutions = this.Mapper.Map<List<TakenQuizInfo>>(user.SolutionsSubmited.Take(10));
 
-            var pageModel = new ProfilePageViewModel();
+            var pageModel = new ProfilePageViewModel
+            {
+                PublicProfile = profileInfo,
+                QuizzesCreated = lastQuizzes,
+                QuizzesTaken = lastSolutions
+            };
 
-            return View(pageModel);
+            return this.View(pageModel);
         }
     }
 }
