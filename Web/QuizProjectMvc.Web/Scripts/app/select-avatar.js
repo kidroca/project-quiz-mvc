a5b632b6ee53c193554e28661b3849a38fdecb0a
Peter Velkov <kidroca@gmail.com>
2016-02-26 22:06:56 +0200

    Added avatar images, styles, and select avatar script

---
+++ Web/QuizProjectMvc.Web/Scripts/app/select-avatar.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    $(document).ready(function () {
+        var selectedAvatarPlaceholder = $('#selected-avatar');
+        var selectedAvatarFormField = $('#AvatarUrl');
+
+        $('div[data-id="avatars"').on('click', 'img', function(ev) {
+            var $this = $(this);
+            var src = $this.attr('src');
+            if (src) {
+                selectedAvatarPlaceholder.attr('src', src);
+                selectedAvatarFormField.val(src);
+            }
+        });
+    });
+})();
