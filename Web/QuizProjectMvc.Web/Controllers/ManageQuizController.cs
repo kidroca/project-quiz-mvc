7eeefde796923acb0d0fb594af3901557ef6d5bb
Peter Velkov <kidroca@gmail.com>
2016-02-27 23:42:42 +0200

    Renamed CreateQuizController to ManagedQuizController

---
+++ Web/QuizProjectMvc.Web/Controllers/ManageQuizController.cs
@@ -0,0 +1,43 @@
+﻿namespace QuizProjectMvc.Web.Controllers
+{
+    using System.Web.Mvc;
+    using Services.Data.Models.Crud.Create;
+    using Services.Data.Protocols;
+
+    public class ManageQuizController : BaseController
+    {
+        private readonly IQuizzesService quizzes;
+
+        public ManageQuizController(IQuizzesService quizzes)
+        {
+            this.quizzes = quizzes;
+        }
+
+        // The Post Method is handled in the api/Quizzes/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        // The Post Method is handled in the api/Quizzes/Update/{id}
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var quiz = this.quizzes.GetById(id);
+            if (quiz == null)
+            {
+                return this.HttpNotFound("The specified quiz has disappeared without a trace");
+            }
+
+            var model = this.Mapper.Map<CreateQuizModel>(quiz);
+
+            return this.View(model);
+        }
+
+        public ActionResult AddQuestionTemplate()
+        {
+            return this.PartialView();
+        }
+    }
+}
