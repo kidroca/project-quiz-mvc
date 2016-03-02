363a73c8288dbcbb077d0f09c559bf4c2c122652
Peter Velkov <kidroca@gmail.com>
2016-03-02 17:12:47 +0200

    Added question "has correct answer" validation

--- Web/QuizProjectMvc.Web/ViewModels/Quiz/Manage/ManageQuestionModel.cs
+++ Web/QuizProjectMvc.Web/ViewModels/Quiz/Manage/ManageQuestionModel.cs
@@ -2,11 +2,12 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
     using Common;
     using Data.Models;
     using Infrastructure.Mapping;
 
-    public class ManageQuestionModel : IMapTo<Question>, IMapFrom<Question>
+    public class ManageQuestionModel : IMapTo<Question>, IMapFrom<Question>, IValidatableObject
     {
         public int Id { get; set; }
 
@@ -20,5 +21,15 @@
         public string ResultDescription { get; set; }
 
         public virtual ICollection<ManageAnswerModel> Answers { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            bool hasCorrectAnswer = this.Answers.Any(a => a.IsCorrect);
+
+            if (!hasCorrectAnswer)
+            {
+                yield return new ValidationResult("A question must have a correct answer");
+            }
+        }
     }
 }
