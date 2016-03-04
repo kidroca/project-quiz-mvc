bcbdeb9341e19d6b62e85039669f08423149d5dd
Peter Velkov <kidroca@gmail.com>
2016-03-04 16:09:33 +0200

    Added About page with controller, removed footer navigation

---
+++ Web/QuizProjectMvc.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizProjectMvc.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
