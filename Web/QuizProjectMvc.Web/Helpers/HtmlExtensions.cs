2cf6f0453ab83673755c3f91dfa04bd45285b864
Peter Velkov <kidroca@gmail.com>
2016-03-04 17:27:04 +0200

    Extracted pager to partial view to be reusable. Created HtmlExtensions for easier obtaining of Controller and Action names

---
+++ Web/QuizProjectMvc.Web/Helpers/HtmlExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace QuizProjectMvc.Web.Helpers
+{
+    using System.Web.Mvc;
+
+    public static class HtmlExtensions
+    {
+        public static string ActionName(this HtmlHelper html)
+        {
+            return html.ViewContext.RouteData.GetRequiredString("action");
+        }
+
+        public static string ControllerName(this HtmlHelper html)
+        {
+            return html.ViewContext.RouteData.GetRequiredString("controller")
+                .Replace("Controller", string.Empty);
+        }
+    }
+}
