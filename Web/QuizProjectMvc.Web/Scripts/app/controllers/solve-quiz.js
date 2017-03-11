ca03c544cac8312d5c703a0db208e6329946dc67
Peter Velkov <kidroca@gmail.com>
2017-03-11 23:39:49 +0200

    refactor(views/solve): updated with transition and pagination highlighting

--- Web/QuizProjectMvc.Web/Scripts/app/controllers/solve-quiz.js
+++ Web/QuizProjectMvc.Web/Scripts/app/controllers/solve-quiz.js
@@ -1,36 +1,46 @@
 ﻿(function(angular, quiz) {
     'use strict';
 
+    var INITIAL_TRANSLATE = 16.5;
+    var TRANSLATE_STEP = 133.45;
+
     if (!angular || !quiz) {
         throw new Error('Solve quiz module has missing dependencies');
     }
 
-    function SolveQuizController($http, $timeout, $document, errorHandler) {
+    angular.module('solveQuiz', ['ui.bootstrap', 'slickCarousel', 'paging', 'errorHandler'])
+           .controller('SolveQuizController', ['$http', '$timeout', 'errorHandler', SolveQuizController])
+           .directive('kidQuestionPagination', ['uibPaginationDirective', questionPagination]);
+
+    function SolveQuizController($http, $timeout, errorHandler) {
 
         var self = this;
 
         self.$http = $http;
         self.$timeout = $timeout;
-        self.$document = $document;
         self.errorHandler = errorHandler;
 
         /**
          * Mark the selected answer of quiz a question
-         * @param {object} question 
+         * @param {int} questionIndex
          * @param {int} selectedAnswerIndex 
          * @returns {void} 
          */
-        self.select = function (question, selectedAnswerIndex) {
+        self.select = function (questionIndex, selectedAnswerIndex) {
+
+            var question = self.quiz.Questions[questionIndex];
 
             var hasPrevSelected = angular.isDefined(question.selected);
 
             question.selected = selectedAnswerIndex;
 
-            self.$questionsPaging.find('.active').addClass('answered');
+            $timeout(function () {
 
-            if (!hasPrevSelected) self.next();
+                self.pager.toggleAnswered(questionIndex + 1, true);
+                self.progress();
 
-            self.progress();
+                if (!hasPrevSelected) self.next();
+            }, 500);
         };
 
         /**
@@ -70,6 +80,22 @@
         self._init();
     }
 
+    Object.defineProperties(SolveQuizController.prototype, {
+
+        currentPage: {
+            get: function () {
+                return this._currentPage;
+            },
+            set: function (pageNumber) {
+
+                var pageDiff = Math.abs(this.currentPage - pageNumber);
+                this.transformTransition = pageDiff < 2 ? 'transform 0.6s' : 'transform 0s';
+
+                this._currentPage = pageNumber;
+            }
+        }
+    });
+
     /**
      * Initializes the properties of the SolveQuizController
      * @private
@@ -85,38 +111,75 @@
         self.questionsCount = quiz.Questions.length;
 
         self.pager = {
-            currentPage: 1,
             pageSize: 1,
             totalPages: self.questionsCount
         };
 
-        self.answeredQuestionsCount = 0;
+        self.currentPage = 1;
 
-        self.$questionsPaging = self.$document.find('.pagination');
+        self.sliderWidth = ((self.questionsCount * 2) - 2) * 70 + 'vw';
     };
 
     SolveQuizController.prototype.next = function () {
 
         if (!this.isLastPage()) {
-            this.pager.currentPage++;
+            this.currentPage++;
         }
     };
 
     SolveQuizController.prototype.prev = function () {
 
         if (!this.isFirstPage()) {
-            this.pager.currentPage--;
+            this.currentPage--;
         }
     };
 
     SolveQuizController.prototype.isLastPage = function () {
-        return this.pager.currentPage === this.pager.totalPages;
+        return this.currentPage === this.pager.totalPages;
     };
 
     SolveQuizController.prototype.isFirstPage = function () {
-        return this.pager.currentPage === 1;
+        return this.currentPage === 1;
     };
 
-    angular.module('solveQuiz', ['ui.bootstrap', 'slickCarousel', 'paging', 'errorHandler'])
-        .controller('SolveQuizController', ['$http', '$timeout', '$document', 'errorHandler', SolveQuizController]);
+    SolveQuizController.prototype.getCurrentTranslate = function () {
+
+        var translate = ((this.currentPage - 1) * -TRANSLATE_STEP) + INITIAL_TRANSLATE;
+        return 'translateX(' + translate + 'vw)';
+    };
+
+    // ### Pagination Extension ###
+
+    function questionPagination(uibPaginationDirective) {
+
+        var config = uibPaginationDirective[0];
+
+        var extension = {
+            name: 'kidQuestionPagination',
+            templateUrl: '/Content/templates/kid-question-paging.html',
+            require: ['kidQuestionPagination', '?ngModel'],
+            link: function (scope, el, attrs) {
+
+                config.link.apply(this, arguments);
+                scope.answered = {};
+
+                scope.toggleAnswered = function (pageNumber, state) {
+
+                    if (angular.isDefined(state)) {
+                        scope.answered[pageNumber] = state;
+                    }
+                    else {
+                        scope.answered[pageNumber] = !scope.answered[pageNumber];
+                    }
+                }
+            }
+        };
+
+        var extended = angular.extend({}, config, extension);
+        extended.scope.toggleAnswered = '=';
+        delete extended.compile;
+
+        return extended;
+    }
+
 })(window.angular, window.quiz);
