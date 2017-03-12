8d13de010456cb4add2c5f64b97bb4e87852b384
Peter Velkov <kidroca@gmail.com>
2017-03-12 12:38:22 +0200

    feat(evaluation/models): new interfaces for quiz evaluation

---
+++ Services/QuizProjectMvc.Services.Data/Models/Evaluation/Contracts/IQuestionResult.cs
@@ -0,0 +1,19 @@
+﻿namespace QuizProjectMvc.Services.Data.Models.Evaluation.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IQuestionResult
+    {
+        string Question { get; }
+
+        int QuestionId { get; }
+
+        int CorrentAnswer { get; }
+
+        int SelectedAnswer { get; }
+
+        bool AnsweredCorrectly { get; }
+
+        IList<IAvailableAnswer> Answers { get; }
+    }
+}
